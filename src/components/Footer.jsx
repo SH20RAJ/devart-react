34530e93a4111196919510ef1ee7712eb78f2d87
sh20raj <shaswatraj3@gmail.com>
2024-08-01 11:15:37 +0530

    refactor: Update footer to include Terabox Video Player link

--- src/components/Footer.jsx
+++ src/components/Footer.jsx
@@ -4,7 +4,7 @@ export default function Footer() {
   return (
     <div>
       <div className="bg-slate-800 text-white text-center p-4">
-        &copy; 2024 DevArt || @sh20raj
+        &copy; 2024 DevArt || @sh20raj || <a href="https://www.terabox.tech/" rel="dofollow  ">Terabox Video Player</a>
       </div>
     </div>
   )
