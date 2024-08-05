9d46e2ee9437d373fbe829e055d8a1272f4e3e5c
sh20raj <shaswatraj3@gmail.com>
2024-08-05 18:30:20 +0530

    refactor: Update footer to include Terabox Video Player and Apped links

--- src/components/Footer.jsx
+++ src/components/Footer.jsx
@@ -4,8 +4,15 @@ export default function Footer() {
   return (
     <div>
       <div className="bg-slate-800 text-white text-center p-4">
-        &copy; 2024 DevArt || @sh20raj || <a href="https://www.terabox.tech/" rel="dofollow  ">Terabox Video Player</a>
+        &copy; 2024 DevArt || @sh20raj ||{" "}
+        <a href="https://www.terabox.tech/" rel="dofollow dorefferer  ">
+          Terabox Video Player
+        </a>{" "}
+        ||{" "}
+        <a href="https://www.apped.me/" rel="dofollow dorefferer  ">
+          Apped - Share, Download Apps
+        </a>
       </div>
     </div>
-  )
+  );
 }
